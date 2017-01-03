49861313b43bd5b4fa114b0d711c63baf6d05e9e
Theresa Palid <tpalid1@gmail.com>
2017-01-03 02:00:21 +0000

    Configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
     grunt.registerTask( 'run', [ 'hapi', 'watch' ]);
 
     grunt.initConfig({
-
+        
         watch: {
             hapi: {
                 files: [
